e6cc7052ba6c50b782c988e835466a8a9737a993
Segfaulter <72930656+brudnevskij@users.noreply.github.com>
2025-01-26 01:08:22 +0200

    Added finite fields methods and tested them

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::format;
+use std::ops::Sub;
 use num_bigint::{BigUint};
 
 struct Point{
@@ -30,16 +32,85 @@ struct FiniteField {}
 
 impl FiniteField {
     fn add(a: &BigUint, b:&BigUint, p: &BigUint)-> BigUint{
-        todo!()
+        let sum = a+b;
+        sum.modpow(&BigUint::from(1u32), &p)
     }
     fn mul(a: &BigUint, b:&BigUint, p: &BigUint)-> BigUint{
-        todo!()
+        let product = a * b;
+        product.modpow(&BigUint::from(1u32), &p)
     }
     fn inv_add(n: &BigUint, p: &BigUint)-> BigUint{
-        todo!()
+        assert!(n<p, "number: {} is bigger or equal than modulus {}", n, p);
+        p - n
     }
 
     fn inv_mul(n: &BigUint, p: &BigUint)-> BigUint{
-        todo!()
+        n.modpow(&(p-BigUint::from(2u32)), p)
+    }
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_add(){
+        let a= BigUint::from(4u32);
+        let b= BigUint::from(10u32);
+        let p= BigUint::from(11u32);
+
+        let sum = FiniteField::add(&a,&b,&p);
+
+        assert_eq!(sum, BigUint::from(3u32));
+    }
+
+    #[test]
+    fn test_add_2(){
+        let a= BigUint::from(4u32);
+        let b= BigUint::from(10u32);
+        let p= BigUint::from(32u32);
+
+        let sum = FiniteField::add(&a,&b,&p);
+
+        assert_eq!(sum, BigUint::from(14u32));
+    }
+
+    #[test]
+    fn test_mul(){
+        let a= BigUint::from(4u32);
+        let b= BigUint::from(10u32);
+        let p= BigUint::from(11u32);
+
+        let prod = FiniteField::mul(&a,&b,&p);
+
+        assert_eq!(prod, BigUint::from(7u32));
+    }
+
+    #[test]
+    fn test_inv_add(){
+        let a= BigUint::from(4u32);
+        let p= BigUint::from(51u32);
+
+        let prod = FiniteField::inv_add(&a,&p);
+
+        assert_eq!(prod, BigUint::from(47u32));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_inv_add_2(){
+        let a= BigUint::from(52u32);
+        let p= BigUint::from(51u32);
+
+        let _ = FiniteField::inv_add(&a,&p);
+    }
+
+    #[test]
+    fn test_inv_mul(){
+        let a= BigUint::from(4u32);
+        let p= BigUint::from(11u32);
+
+        let prod = FiniteField::inv_mul(&a,&p);
+
+        assert_eq!(prod, BigUint::from(3u32));
     }
 }
